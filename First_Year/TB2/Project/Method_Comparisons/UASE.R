1face058b22983519019b2bce815419f2712f229
Rachel Wood <rachel.m.wood@bristol.ac.uk>
2023-04-14 17:29:31 +0100

    backup Fri 14 Apr 17:29:31 BST 2023

---
+++ First_Year/TB2/Project/Method_Comparisons/UASE.R
@@ -0,0 +1,75 @@
+require(ggplot2)
+require(dplyr)
+require(gridExtra)
+
+UASE <- function(sim1, sim2,d = 2){
+  Y<- cbind(sim1$Y, sim2$Y)
+  
+  svd <- svd(Y)
+  
+  if (!all.equal(sim1$A, sim2$A)){
+    stop("Community matrices must be the same")
+  }
+  
+  A <- sim1$A
+  population <- A %*% 1:ncol(A)
+  
+  
+  U <- svd$u %*% diag(sqrt(svd$d))
+  U <- U[, 1:d]
+  left <- as.data.frame(cbind(U, as.factor(population)))
+  colnames(left) <- c("1", "2", "population")
+  
+  t <- rep(1:2, each = nrow(A))
+  index <- rep(1:nrow(A),2)
+  V <- svd$v %*% diag(sqrt(svd$d))
+  V <- V[,1:d]
+  right <- as.data.frame(cbind(V,as.factor(t), 
+                               as.factor(population), 
+                               as.factor(index)))
+  
+  colnames(right) <- c("1", "2"," discipline", "population", "observation")
+  
+  return(list(left = left, right = right))
+}
+
+plot_UASE <- function(UASE){
+  
+  U <- UASE$left %>%
+    as_tibble() %>%
+    mutate(population = as.factor(population))
+  
+  V <- UASE$right
+  V1 <- V[1:(nrow(V)/2),] %>%
+    as_tibble() %>%
+    mutate(population = as.factor(population))
+  
+  
+  V2 <- V[-(1:(nrow(V)/2)),] %>%
+    as_tibble() %>%
+    mutate(population = as.factor(population))
+  
+  V1_plot <- ggplot(V1, aes(`1`,`2`, color = population)) +
+    geom_point(alpha = 0.5) +
+    geom_jitter() +
+    scale_color_viridis_d(option = "turbo") + 
+    theme(legend.position = "none") +
+    labs(title = "V Discipline 1")
+  
+  V2_plot <- ggplot(V2, aes(`1`,`2`, color = population)) +
+    geom_point(alpha = 0.5) +
+    geom_jitter() +
+    scale_color_viridis_d(option = "turbo") + 
+    theme(legend.position = "none") +
+    labs(title = "V Discipline 2")
+  
+  U_plot <- ggplot(U, aes(`1`,`2`, color = population)) +
+    geom_point(alpha = 0.5) +
+    geom_jitter() +
+    scale_color_viridis_d(option = "turbo", guide = guide_legend(ncol = 3, label.theme = element_text(size= 8))) +
+    labs(title = "U")
+  
+  lay <- rbind(c(1,2),
+               c(3,3))
+  grid.arrange(V1_plot, V2_plot, U_plot, layout_matrix = lay)
+}
