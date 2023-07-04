e14deecfc6a4356a6e434354633de5c07634abec
Rachel Wood <rachel.m.wood@bristol.ac.uk>
2023-07-04 13:45:20 +0100

    backup Tue  4 Jul 13:45:20 BST 2023

--- First_Year/TB2/Project/Method_Comparisons/distances.R
+++ First_Year/TB2/Project/Method_Comparisons/distances.R
@@ -1,25 +1,31 @@
+require(dplyr)
+require(ggplot2)
 
-distance_moved <- function(V, d, l =2, scale=TRUE, eigenvals = NULL){
-  
-  if(scale){
-    V[,1:d] <-as.matrix(V[,1:d]) %*% diag(1/sqrt(as.numeric(eigenvals[1:d])), nrow = d)
+
+distance_moved <- function(V, d, l = 2, scale = TRUE, eigenvals = NULL) {
+  if (scale) {
+    V[,1:d] <- as.matrix(V[,1:d]) %*% # nolint
+    diag(1 / sqrt(as.numeric(eigenvals[1:d])), nrow = d)
   }
   V <- V %>% as_tibble()
-  
-  n <- nrow(V) 
-  
-  V1 <- V[1:(n/2),1:d]
-  V2 <- V[(n/2 +1):n, 1:d]
+  n <- nrow(V)
+  V1 <- V[1:(n / 2), 1:d]
+  V2 <- V[(n / 2 + 1):n, 1:d]
+
   moved <- sapply(1:(n/2), FUN = function(i){
-    dist(rbind(V1[i,], V2[i,]), method = "minkowski", p =l)
+    dist(rbind(V1[i, ], V2[i, ]), method = "minkowski", p = l)
   })
-  
   return(moved)
 }
 
-plot_distances <- function(UASE, d){
-  distances <- apply(as.matrix(1:d, nrow = 1), MARGIN = 1, FUN = function(ii){
-    distance_moved(mixed_UASE, d = ii)
-  })
+plot_distances <- function(distances, d) {
+  distance_plot <- expand.grid(
+    Components = (1:12),
+    Observations = 1:nrow(distances)) # nolint
+  distance_plot$Distances <- as.numeric(c(t(distances))) %>%
+    as_tibble()
+  plot <- ggplot(distance_plot, aes(x = Components)) +
+    geom_tile(aes(y = Observations, fill = Distances)) #+
+    #scale_fill_distiller(direction = 1)
+  return(plot)
 }
-
