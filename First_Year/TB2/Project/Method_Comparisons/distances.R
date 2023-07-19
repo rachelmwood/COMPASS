591016c420f968821d09b44f304cbab1d69ca80b
Rachel Wood <rachel.m.wood@bristol.ac.uk>
2023-07-19 16:50:34 +0100

    backup Wed 19 Jul 16:50:34 BST 2023

--- First_Year/TB2/Project/Method_Comparisons/distances.R
+++ First_Year/TB2/Project/Method_Comparisons/distances.R
@@ -2,7 +2,7 @@ require(dplyr)
 require(ggplot2)
 
 
-distance_moved <- function(V, d, l = 2, scale = TRUE, eigenvals = NULL) {
+dim_distance <- function(V, d, l = 2, scale = TRUE, eigenvals = NULL) {
   if (scale) {
     V[,1:d] <- as.matrix(V[,1:d]) %*% # nolint
     diag(1 / sqrt(as.numeric(eigenvals[1:d])), nrow = d)
@@ -11,20 +11,31 @@ distance_moved <- function(V, d, l = 2, scale = TRUE, eigenvals = NULL) {
   n <- nrow(V)
   V1 <- V[1:(n / 2), 1:d]
   V2 <- V[(n / 2 + 1):n, 1:d]
-
   moved <- sapply(1:(n/2), FUN = function(i){
     dist(rbind(V1[i, ], V2[i, ]), method = "minkowski", p = l)
   })
   return(moved)
 }
 
-group_distances <- function(distances, d, groups) {
+distance_moved <- function(mat, maxd, scale = TRUE){
+  distances <- apply(
+    as.matrix(1:maxd, nrow = 1),
+    MARGIN = 1,
+    FUN = function(ii) {
+        dim_distance(mat, d = ii, scale = scale)
+    })
   distances_grid <- expand.grid(
-    Components = as.factor(1:d),
+    Components = as.factor(1:maxd),
     Observations = as.factor(1:nrow(distances)))
-  distances_grid$Group <- as.factor(rep(groups, each = 12))
+  distances_grid$Group <- as.factor(rep(groups, each = maxd))
   distances_grid$Distances <- c(t(distances))
   distances_grid <- distances_grid %>%
+    as_tibble()
+  return(distances_grid)
+}
+
+group_distances <- function(distances) {
+  distances_grid <- distances %>%
     as_tibble() %>%
     group_by(Group, Components) %>%
     summarise(Distances = mean(Distances))
--- First_Year/TB2/Project/Method_Comparisons/rocs.R
+++ First_Year/TB2/Project/Method_Comparisons/rocs.R
@@ -9,31 +9,41 @@ roc_data <- function(score, classes, groups = NULL) {
         FPR = roc_fit$FPR,
         TPR = roc_fit$TPR))
     }
-    
+    groups <- as.factor(groups)
     levels <- levels(groups)
-    return_data <- data.frame(
-        FPR = numeric(),
-        TPR = numeric(),
-        grouping = factor()
-    )
-    for (i in levels) {
-        current_scores <- score[groups == i]
-        current_classes <- classes[groups == i]
+    fits <- vector("list", length(levels))
+    names(fits) <- levels
+    for (i in 1:length(levels)) {
+        current_scores <- score[groups == levels[i]]
+        current_classes <- classes[groups == levels[i]]
         current_roc <- rocit(current_scores, current_classes)
-        return_data <- rbind(return_data, data.frame(
-            FPR = current_roc$FPR,
-            TPR = current_roc$TPR,
-            grouping = factor(i)
-        ))
+        fits[[i]] <- current_roc
     }
-    return(return_data)
+    return(fits)
 }
 
-plot_rocs <- function(data, grouping) {
-    plot <- ggplot(data, aes(x = FPR, y = TPR)) + # nolint
-        geom_line(aes(color = grouping)) +
-        geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-        scale_color_brewer(palette = "Set1") +
-        theme_bw()
-    return(plot)
+plot_rocs <- function(
+    roc_list, together = TRUE, layout = NULL, pallete = NULL) {
+    if (together) {
+        if (is.null(pallete)) {
+            pallete <- rainbow(length(roc_list))
+        }
+        plot(roc_list[[1]], col = c(pallete[1], "#BEBEBE"), main = "ROC Curves",
+         legend = FALSE, YIndex = FALSE)
+        for (i in 2:length(roc_list)) {
+            lines(roc_list[[i]]$FPR, roc_list[[i]]$TPR, col = pallete[i])
+        }
+        legend("bottomright", col = pallete,
+            names(roc_list), lwd = 2)
+        return(NULL)
+    } else {
+        if (is.null(layout)) {
+            layout <- matrix(1:length(roc_list), ncol = 2)
+        }
+        par(mfrow = c(nrow(layout), ncol(layout)))
+        for (i in 1:length(roc_list)) {
+            plot(roc_list[[i]], main = names(roc_list)[i])
+        }
+        return(NULL)
+    }
 }
